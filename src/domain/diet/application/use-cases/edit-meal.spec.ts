eba268862aa54c069c6bd2dacb87c2ee06646568
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-10-02 16:27:29 -0300

    test: edit meal, should not be able to edit a meal from another user

--- src/domain/diet/application/use-cases/edit-meal.spec.ts
+++ src/domain/diet/application/use-cases/edit-meal.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryMealsRepository } from 'test/in-memory-meals-repository';
 import { EditMealUseCase } from './edit-meal';
 import { makeMeal } from 'test/factories/make-meal';
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
 
 let inMemoryMealsRepository: InMemoryMealsRepository;
 let sut: EditMealUseCase;
@@ -40,5 +41,29 @@ describe('Edit Meal', () => {
       hour: '19:00:00',
       isOnTheDiet: false,
     });
+  });
+
+  it('should not be able to edit a meal from antoher user', async () => {
+    const newMeal = makeMeal(
+      {
+        userId: new UniqueEntityId('user-1'),
+      },
+      new UniqueEntityId('meal-1'),
+    );
+
+    await inMemoryMealsRepository.create(newMeal);
+
+    const result = await sut.execute({
+      mealId: newMeal.id.toValue(),
+      userId: 'user-2',
+      name: 'Test meal',
+      description: 'Test description',
+      date: '2023-10-10',
+      hour: '19:00:00',
+      isOnTheDiet: false,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
