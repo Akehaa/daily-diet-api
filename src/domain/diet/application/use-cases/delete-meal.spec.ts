f67646662b9405e23535b853e92f35df0311b474
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-10-02 16:38:27 -0300

    unit test: delete meal, should not be able to delete a meal from another user

--- src/domain/diet/application/use-cases/delete-meal.spec.ts
+++ src/domain/diet/application/use-cases/delete-meal.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryMealsRepository } from 'test/in-memory-meals-repository';
 import { DeleteMealUseCase } from './delete-meal';
 import { makeMeal } from 'test/factories/make-meal';
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
 
 let inMemoryMealsRepository: InMemoryMealsRepository;
 let sut: DeleteMealUseCase;
@@ -28,5 +29,24 @@ describe('Delete Meal', () => {
     });
 
     expect(inMemoryMealsRepository.items).toHaveLength(0);
+  });
+
+  it('should not be able to delete a meal from antoher user', async () => {
+    const newMeal = makeMeal(
+      {
+        userId: new UniqueEntityId('user-1'),
+      },
+      new UniqueEntityId('meal-1'),
+    );
+
+    await inMemoryMealsRepository.create(newMeal);
+
+    const result = await sut.execute({
+      mealId: newMeal.id.toValue(),
+      userId: 'user-2',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
