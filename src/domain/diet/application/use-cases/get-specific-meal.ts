f557a3d959c7fb924cdab1eb9f76e80c91c4daf5
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-10-04 11:34:38 -0300

    use case: get-specific-meal

---
+++ src/domain/diet/application/use-cases/get-specific-meal.ts
@@ -0,0 +1,33 @@
+import { Either, left, right } from '@/core/either';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { Meal } from '../../enterprise/entities/meal';
+import { MealsRepository } from '../repositories/meals-repository';
+
+interface GetSpecificMealUseCaseRequest {
+  mealId: string;
+}
+
+type GetSpecificMealUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    meal: Meal;
+  }
+>;
+
+export class GetSpecificMealUseCase {
+  constructor(private mealsRepository: MealsRepository) {}
+
+  async execute({
+    mealId,
+  }: GetSpecificMealUseCaseRequest): Promise<GetSpecificMealUseCaseResponse> {
+    const meal = await this.mealsRepository.findById(mealId);
+
+    if (!meal) {
+      return left(new ResourceNotFoundError());
+    }
+
+    return right({
+      meal,
+    });
+  }
+}
