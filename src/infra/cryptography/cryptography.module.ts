a5afdb0eb1d4f71ee70353078e93f8f50c87d38d
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-10-05 15:45:20 -0300

    fix: cryptography.module.ts

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { JwtEncrypter } from './jwt-encrypter';
 import { BcryptHasher } from './bcrypt-hasher';
-import { Encrypter } from '@/domain/diet/application/cryptography/encrypter';
 import { HashComparer } from '@/domain/diet/application/cryptography/hash-comparer';
 import { HashGenerator } from '@/domain/diet/application/cryptography/hash-generator';
 
 @Module({
   providers: [
-    { provide: Encrypter, useClass: JwtEncrypter },
     { provide: HashComparer, useClass: BcryptHasher },
     { provide: HashGenerator, useClass: BcryptHasher },
   ],
-  exports: [Encrypter, HashComparer, HashGenerator],
+  exports: [HashComparer, HashGenerator],
 })
 export class CryptographyModule {}
